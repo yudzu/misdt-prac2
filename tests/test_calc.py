6765ea535b0ef64ae89452833cb23d47b46b9235
steph <van.stepa@mail.ru>
2025-10-20 16:22:22 +0300

    code refactoring

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,17 +1,22 @@
 import pytest
 from src.calc import add, divide, multiply, sub
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_sub():
     assert sub(5, 3) == 2
 
+
 def test_mul():
     assert multiply(4, 3) == 12
 
+
 def test_divide_normal():
     assert divide(10, 2) == 5
+
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
